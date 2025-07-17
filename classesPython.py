d5a4cacd262749b57e0aa924c6db0677c28a8d1b
Nicolas-guilheme <nicolasrenovado12@gmail.com>
2025-07-17 10:39:13 -0300

    Começando projeto, aprendendo classes e um construtor em python

---
+++ classesPython.py
@@ -0,0 +1,17 @@
+# Parar criar uma classe em python usamos: 
+
+class Classe: 
+    x = 3
+    n1 = 4 
+    n2 = 5 
+    n3 = 6 
+    n4 = 7 
+
+p1 = Classe() # Instanciando a classe em objeto 
+print(p1.x) # Pegando a váriavel da classe. 
+
+# Outras váriaveis da classe:
+print(p1.n1)
+print(p1.n2) 
+print(p1.n3)
+print(p1.n4)
---
+++ oQueédefinit.py
@@ -0,0 +1,12 @@
+# O def __init__ permite que definamos os atributos da classe ao cria-la. Basicamente poderia ser comparado ao um funcionamento de um construtor
+
+class Animal:
+    def __init__(self, nome, cor):
+        self.nome = nome 
+        self.cor = cor 
+        pass
+
+p1 = Animal("Nicolas", "Azul")
+
+print(p1.nome)
+print(p1.cor)
